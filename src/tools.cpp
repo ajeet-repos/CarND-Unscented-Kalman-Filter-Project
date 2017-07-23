fe52a93535e6b7d4e0a5ceacec668d90aacaa58f
Ajeet Kumar <ajeet.ajeetkumar@gmail.com>
2017-07-24 01:31:26 +0530

    [Added] 1. code to calculate the RMSE values for the filter

--- src/tools.cpp
+++ src/tools.cpp
@@ -15,4 +15,26 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+
+  VectorXd rmse = VectorXd(4);
+  rmse << 0, 0, 0, 0;
+
+
+  int estimationsSize = estimations.size();
+  if (estimationsSize != ground_truth.size() || estimationsSize == 0) {
+    return rmse;
+  }
+
+  for(int i = 0; i < estimationsSize; i++) {
+
+    VectorXd error = estimations[i] - ground_truth[i];
+    error = error.array() * error.array();
+    rmse += error;
+  }
+
+  rmse = rmse / estimationsSize;
+  rmse = rmse.array().sqrt();
+
+  return rmse;
+
 }
