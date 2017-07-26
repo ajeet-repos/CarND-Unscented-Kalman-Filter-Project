fac42c124b9962a3ccbd9a5fea074d841093c378
Ajeet Kumar <ajeet.ajeetkumar@gmail.com>
2017-07-26 23:13:58 +0530

    [Added] 1. initialization and process management code.

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -51,6 +51,19 @@ UKF::UKF() {
 
   Hint: one or more values initialized above might be wildly off...
   */
+
+  n_x_ = 5;
+  n_aug_ = 7;
+  Xsig_pred_ = MatrixXd(n_x_, 2*n_aug_ + 1);
+  weights_ = VectorXd(2* n_aug_ + 1);
+  lambda_ = 3 - n_aug_;
+  P_ << 1, 0, 0, 0
+        0, 1, 0, 0
+        0, 0, 1, 0
+        0, 0, 0, 1;
+
+  is_initialized_ = false;
+
 }
 
 UKF::~UKF() {}
@@ -66,6 +79,52 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
   Complete this function! Make sure you switch between lidar and radar
   measurements.
   */
+  if(!is_initialized_) {
+
+    if(meas_package.sensor_type_ == MeasurementPackage::RADAR) {
+      // sensor is RADAR
+      float rho = meas_package.raw_measurements_[0];
+      float theta = meas_package.raw_measurements_[1];
+
+      float px = rho * cos(theta);
+      float py = rho * sin(theta);
+      float v = 0.0;
+      float phi = 0.0;
+      float phi_dot = 0.0;
+
+      x_ << px, py, v, phi, phi_dot;
+
+    } else {
+      // sendor is LIDAR
+
+      float px = meas_package.raw_measurements_[0];
+      float py = meas_package.raw_measurements_[1];
+      float v = 0.0;
+      float phi = 0.0;
+      float phi_dot = 0.0;
+
+      x_ << px, py,  v, phi, phi_dot;
+
+    }
+
+    time_us_ = meas_package.timestamp_;
+    is_initialized_ = true;
+    return;
+  }
+
+  // delta time in seconds
+  float delta_t = (meas_package.timestamp_ - time_us_) / 1000000.0;
+  time_us_ = meas_package.timestamp_;
+
+  // predict
+  Prediction(delta_t);
+
+  if(meas_package.sensor_type_ == MeasurementPackage::RADAR){
+    UpdateRadar(meas_package);
+  }else{
+    UpdateLidar(meas_package);
+  }
+
 }
 
 /**
